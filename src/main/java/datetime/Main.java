802296161a607b4671fd5bb4f391ad702b79bd78
mariusz.lewandowski <m.lewandowski@nuadu.com>
2018-11-26 15:21:28 +0100

    new

---
+++ src/main/java/datetime/Main.java
@@ -0,0 +1,14 @@
+package datetime;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        DateTimeCheck c = new DateTimeCheck();
+
+        c.truncateToMinutes();
+
+    }
+
+
+}
