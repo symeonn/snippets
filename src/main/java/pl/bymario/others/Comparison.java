aa2164101572a9d425940cb4efe31c631900ca55
mariusz.lewandowski <m.lewandowski@nuadu.com>
2020-06-04 14:26:34 +0200

    +str comarison

--- src/main/java/pl/bymario/others/Comparison.java
+++ src/main/java/pl/bymario/others/Comparison.java
@@ -1,37 +1,74 @@
 package pl.bymario.others;
 
+import lombok.extern.java.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log
 public class Comparison {
 
     public static void main(String[] args) {
 
-        ClassOne one = new ClassOne();
-        ClassOne two = new ClassOne();
-        ClassOne three = one;
+//        ClassOne one = new ClassOne();
+//        ClassOne two = new ClassOne();
+//        ClassOne three = one;
+//
+//        System.out.println(one == two);
+//        System.out.println(one.equals(two));
+//
+//        System.out.println(one == three);
+//        System.out.println(one.equals(three));
+//
+//
+//        System.out.println(EnumOne.ENUM_ONE == EnumOne.ENUM_ONE);
+//        System.out.println(EnumOne.ENUM_ONE.equals(EnumOne.ENUM_ONE));
+//
+//        System.out.println(EnumOne.ENUM_ONE == EnumOne.ENUM_TWO);
+//        System.out.println(EnumOne.ENUM_ONE.equals(EnumOne.ENUM_TWO));
+//
+//
+//        String strOne = "one";
+//
+//        System.out.println(strOne == "one");
+//        System.out.println(strOne.equals("one"));
 
-        System.out.println(one == two);
-        System.out.println(one.equals(two));
+        lambdaComparison();
 
-        System.out.println(one == three);
-        System.out.println(one.equals(three));
+    }
 
 
-        System.out.println(EnumOne.ENUM_ONE == EnumOne.ENUM_ONE);
-        System.out.println(EnumOne.ENUM_ONE.equals(EnumOne.ENUM_ONE));
+    static void lambdaComparison(){
+        List<StringTuple> strings = new ArrayList<>();
+        strings.add(new StringTuple("one", "one"));
+        strings.add(new StringTuple("one", "two"));
+        strings.add(new StringTuple("two", "two"));
+        strings.add(new StringTuple("one", "three"));
 
-        System.out.println(EnumOne.ENUM_ONE == EnumOne.ENUM_TWO);
-        System.out.println(EnumOne.ENUM_ONE.equals(EnumOne.ENUM_TWO));
+//        List<StringTuple> collect =
+                strings.stream()
+//                .filter(stringTuple -> stringTuple.first == stringTuple.second)
+                .forEach(stringTuple -> log.info(String.valueOf(stringTuple.first == stringTuple.second)));
+//                .collect(Collectors.toList());
 
+        log.info("d");
 
+    }
+    static class StringTuple {
+        public StringTuple(String first, String second) {
+            this.first = first;
+            this.second = second;
+        }
 
+        private String first;
+        private String second;
     }
 
     static class ClassOne {
         private int fieldInt;
         private String fieldStr;
         private RuntimeException fieldExc;
-
-
-
     }
 
 
